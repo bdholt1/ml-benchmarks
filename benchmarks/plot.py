21b29ec9c94c02cc7ff7c6592d157765b19cb9ef
Brian Holt <bdholt1@gmail.com>
2011-09-07 10:14:22 +0100

    display table of timings and scores in rst format

--- benchmarks/plot.py
+++ benchmarks/plot.py
@@ -59,9 +59,81 @@ def plot_results_for_task(task, datasets, packages, scores, means, stds):
 
     plt.show()
     plt.savefig("bench_%s.png" % (task))
+ 
+ 
+def hcat(left, right, spaces=""):
+    res = []
+    for l, r in zip(left,right):
+        res.append(l + spaces + r)
+    return res
 
 
-def bar_plot(task):
+def frame(top, bottom):
+    # calculate the max length of all the strings
+    max_len = len(top)
+    for b in bottom:
+        if len(b) > max_len:
+            max_len = len(b)
+    
+    f = ["="*max_len]
+    spaces = " "*(max_len - len(top))
+    f.append(spaces + top)
+    f.append("="*max_len)
+    for b in bottom:
+        spaces = " "*(max_len - len(b))
+        f.append(spaces + b)
+    f.append("="*max_len) 
+    
+    return f
+
+
+def rst_table(task, datasets, packages, values, use_min=True):
+    """Print the results in a table like this one:
+    
+    ============    =======     ======     ======     =======     ========    =============      ========
+         Dataset     PyMVPA     Shogun        MDP     Pybrain         MLPy     scikit-learn          Milk
+    ============    =======     ======     ======     =======     ========    =============      ========
+         Madelon      11.52       5.63      40.48        17.5         9.47         **5.20**          5.76
+         Arcene        1.30       0.39       4.87          --         1.61             0.38      **0.33**
+    ============    =======     ======     ======     =======     ========    =============      ========
+    """
+    import math
+    
+    a = "Dataset"
+    b = datasets
+    output = frame(a, b)
+    
+    value_strings = []
+    for v in values:
+        # turn the values into strings
+        vs_temp = []
+        m = np.inf
+        for v2 in v:
+            x = float(v2)
+            if not math.isnan(x) and x < m:
+                m = x 
+        for v2 in v:
+            x = float(v2)
+            if math.isnan(x) or math.isinf(x):
+                vs_temp.append("--")
+            elif use_min and v2 == m:
+                vs_temp.append("**%.02f**" % v2)
+            else:
+                vs_temp.append("%.02f" % v2)
+        value_strings.append(vs_temp)
+    
+    # transpose the value strings list of lists so that we can work on
+    # columns
+    values = [list(v) for v in zip(*value_strings)]
+    
+    for a, b in zip(packages, values):
+        o = frame(a, b)
+        output = hcat(output, o, " "*4)
+
+    return output
+
+
+def prepare_results(task):
 
     import glob
     result_files = glob.glob("%s*.results" % (task))
@@ -90,6 +162,18 @@ def bar_plot(task):
 
     plot_results_for_task(task, datasets, packages,
                           scores, means, stds)
+    rst = rst_table(task, datasets, packages, means, use_min=True)
+    print "Timing for ", task
+    for l in rst:
+        print l
+    
+    print
+    rst = rst_table(task, datasets, packages, scores, use_min=False)
+    print "Scores for ", task
+    for l in rst:
+        print l    
+    print
+    
 
 USAGE = """usage: python plot.py package
 
@@ -111,4 +195,4 @@ if __name__ == "__main__":
     else:
         task = sys.argv[1]
 
-    bar_plot(task)
+    prepare_results(task)
